72f881447a6817a62c9e8c1533008d8d436d1bc1
Vladimir Borisov <borisov1024@gmail.com>
2026-03-12 17:27:44 +0200

    Remove sign thread

--- build.ps1
+++ build.ps1
@@ -1,21 +1,2 @@
-param (
-    [String]$pw = $( Read-Host "Password" )
-)
-
-$thread = Start-ThreadJob -InputObject ($pw) -ScriptBlock {
-    $wshell = New-Object -ComObject wscript.shell;
-    $pw = "$($input)~"
-    while ($true) {
-        while ( -not $wshell.AppActivate("Token Logon")) {
-            Start-Sleep 1
-        }
-        Start-Sleep 1
-        $wshell.SendKeys($pw, $true)
-        Start-Sleep 1
-    }
-}
-
 cargo build --release --target x86_64-pc-windows-msvc
 & "C:\Program Files (x86)\Inno Setup 6\ISCC.exe" /DSIGN "/Sstremiosign=`$qsigntool`$q sign /fd SHA256 /t http://timestamp.digicert.com /n `$qSmart Code OOD`$q `$f" "setup\Stremio.iss"
-
-Stop-Job -Job $thread
